823f146c6bfebc202de7056beff295b785c550ac
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-05-20 02:03:34 +0530

    cache the login form and logout button from the DOM

--- controller/LoginController.js
+++ controller/LoginController.js
@@ -1,1 +1,11 @@
 import db from '../db/db.js';
+
+class LoginController {
+    constructor() {
+
+        // Cache the login form and logout button from the DOM
+        this.loginForm = document.getElementById('login-form');
+        this.logoutBtn = document.getElementById('logout-btn');
+    }
+
+}
