94568f2aa5bbfdf95b6d14727f2a4d4266b55aa0
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-05-20 01:39:20 +0530

    import db and controller modules for page logic

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
 
+import db from './db/db.js';
+import LoginController from './controller/LoginController.js';
+import CustomerController from './controller/CustomerController.js';
+import ItemController from './controller/ItemController.js';
+import OrderController from './controller/OrderController.js';
 
 window.showAlert = function(message, type) {
     const alertDiv = document.createElement('div');
