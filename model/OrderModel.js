42c06762903c94484455a9964e7d29f0e394f9fc
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-05-20 19:41:04 +0530

    add validate() method to check required fields and value constraints

--- model/OrderModel.js
+++ model/OrderModel.js
@@ -19,7 +19,21 @@ class OrderModel {
         this.balance = parseFloat(balance) || 0;
     }
 
+    validate() {
+        if (!this.orderId) throw new Error('Order ID is required');
+        if (!this.customerId) throw new Error('Customer ID is required');
+        if (this.total < 0) throw new Error('Total cannot be negative');
+        if (this.discount < 0) throw new Error('Discount cannot be negative');
+        if (this.subTotal < 0) throw new Error('Subtotal cannot be negative');
+        if (this.cash < 0) throw new Error('Cash amount cannot be negative');
 
+        // Validate discount doesn't exceed subtotal
+        if (this.discount > this.subTotal) {
+            throw new Error('Discount cannot exceed subtotal');
+        }
+
+
+
+    }
 }
-
 export default OrderModel;
