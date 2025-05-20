235044a78215783c6642eceb5c15157349971737
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-05-20 19:47:16 +0530

    add validate() method with field checks

--- model/OrderDetailsModel.js
+++ model/OrderDetailsModel.js
@@ -15,4 +15,15 @@ class OrderDetailsModel {
         this.total = parseFloat(total) || 0;
     }
 
+    validate() {
+        if (!this.orderId) throw new Error('Order ID is required');
+        if (!this.itemId) throw new Error('Item ID is required');
+        if (!this.itemName) throw new Error('Item name is required');
+        if (this.quantity <= 0) throw new Error('Quantity must be positive');
+        if (this.unitPrice < 0) throw new Error('Unit price cannot be negative');
+        if (this.total < 0) throw new Error('Total cannot be negative');
+
+
+    }
+
 }
