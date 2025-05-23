9f537cf248c987d80b53d37fd401b4a271290c46
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-05-23 19:43:17 +0530

    create customer controller class with selected index

---
+++ controller/CustomerController.js
@@ -0,0 +1,13 @@
+import { customer_db } from "../db/db.js";
+import CustomerModel from "../model/CustomerModel.js";
+
+class CustomerController {
+    constructor() {
+        this.selectedIndex = -1;
+        this.initialize();
+    }
+
+
+}
+
+export default CustomerController;
