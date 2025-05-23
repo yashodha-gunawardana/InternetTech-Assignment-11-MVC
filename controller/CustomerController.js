3ff269be9375dda4e2cc70b15177168238013897
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-05-23 19:45:06 +0530

    bind all customer-related button and table events

--- controller/CustomerController.js
+++ controller/CustomerController.js
@@ -13,6 +13,17 @@ class CustomerController {
         this.setupValidation();
     }
 
+    bindEvents() {
+        $('#customer-save').on("click", () => this.handleSave());
+        $('#cus-body').on('click', 'tr', (e) => this.handleRowClick(e));
+        $('#customer-clear').on('click', () => this.clearForm());
+        $('#customer-update').on('click', () => this.handleUpdate());
+        $('#customer-delete').on('click', () => this.handleDelete());
+        $('.search-bar').on('input', () => this.handleSearch());
+        $('.btn-view-all').on('click', () => this.loadCustomerTableData());
+        $('.btn-go-back').on('click', () => window.history.back());
+    }
+
 
 }
 
