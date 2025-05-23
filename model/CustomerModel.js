4e145a06e746299475bf78f6db0f8e98641880b6
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-05-23 19:41:31 +0530

    define customer model class with basic customer attributes

---
+++ model/CustomerModel.js
@@ -0,0 +1,9 @@
+class CustomerModel {
+    constructor(customerId, customerName, address, phoneNumber) {
+        this.customerId = customerId;
+        this.customerName = customerName;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+}
+export default CustomerModel;
