1ffb3d8f31ce483b71bf8c495701dde797b71529
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-05-23 19:55:11 +0530

    define itemModel class with basic item attributes

---
+++ model/ItemModel.js
@@ -0,0 +1,9 @@
+class ItemModel {
+    constructor(itemId, itemName, quantity, price) {
+        this.itemId = itemId;
+        this.itemName = itemName;
+        this.quantity = quantity;
+        this.price = price;
+    }
+}
+export default ItemModel;
