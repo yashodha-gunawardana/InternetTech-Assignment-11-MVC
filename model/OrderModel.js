956e785f9cec8cfa52ca4e28ae2501fe327d4128
yashodha-25 <yashodagunawardhana15@gmail.com>
2025-05-23 20:04:34 +0530

    add order model attributes

---
+++ model/OrderModel.js
@@ -0,0 +1,19 @@
+// orderModel.js
+
+class Order {
+    constructor(orderId, orderDate, itemId, itemName, customerId, customerName, availableQty, orderQty, unitPrice) {
+        this.orderId = orderId;               // Order ID
+        this.orderDate = orderDate;           // Order Date (YYYY-MM-DD)
+        this.itemId = itemId;                 // Item ID (from select)
+        this.itemName = itemName;             // Item Name (text)
+        this.customerId = customerId;         // Customer ID (from select)
+        this.customerName = customerName;     // Customer Name (text)
+        this.availableQty = availableQty;     // Available quantity of item (readonly)
+        this.orderQty = orderQty;             // Quantity ordered
+        this.unitPrice = unitPrice;           // Unit price of the item
+    }
+
+
+}
+
+export default Order;
